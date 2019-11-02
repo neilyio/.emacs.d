500cbe81a100b4a8d47f87154b8fc0f475bd0d25
Neil Hansen <neil@Neils-MacBook-Air.local>
2019-11-01 20:47:03 -0700

    Creates file for custom config, starting with putting META on ALT key.

---
+++ lisp/init-local.el
@@ -0,0 +1,11 @@
+;;; init-local.el --- Custom User Settings -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(setq mac-option-key-is-meta t)
+(setq mac-command-key-is-meta nil)
+(setq mac-command-modifier nil)
+(setq mac-option-modifier 'meta)
+
+(provide 'init-local)
+;;; init-local.el ends here
